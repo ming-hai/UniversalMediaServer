d4a76e584d4b7efb604c18cd02bbb35c8a2832f2
Nadahar <Nadahar@users.noreply.github.com>
2017-01-14 16:38:14 +0100

    - Refactored Exif orientation handling:   - Removed AutoExifRotate renderer configuration parameter   - Removed mediautil as a dependency   - Moved parseExifOrientation() from ImageInfo to ImagesUtil   - Changed default/null Exif orientation from 0 to 1 (bug)   - Created ExifOrientation enum   - Removed ImagesUtil.getAutoRotateInputStreamImage()   - Removed ImagesUtil.auto()   - Made ImagesUtil.transcodeImage() and all its overloads/variants rotate and flip the image according to Exif orientation automatically. Exif orientation metadata is removed/reset so the operation won't be repeated neither internally nor externally.

--- src/main/java/net/pms/dlna/DLNAMediaInfo.java
+++ src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -35,6 +35,7 @@ import net.pms.io.ProcessWrapperImpl;
 import net.pms.network.HTTPResource;
 import net.pms.util.CoverSupplier;
 import net.pms.util.CoverUtil;
+import net.pms.util.ExifOrientation;
 import net.pms.util.FileUtil;
 import net.pms.util.ImagesUtil;
 import net.pms.util.ImagesUtil.ScaleType;
@@ -2239,6 +2240,10 @@ public class DLNAMediaInfo implements Cloneable {
 	 */
 	public void setImageInfo(ImageInfo imageInfo) {
 		this.imageInfo = imageInfo;
+		if (imageInfo != null && imageInfo.getWidth() > 0 && imageInfo.getHeight() > 0) {
+			setWidth(imageInfo.getWidth());
+			setHeight(imageInfo.getHeight());
+		}
 	}
 
 	/**
@@ -2397,11 +2402,11 @@ public class DLNAMediaInfo implements Cloneable {
 	}
 
 	/**
-	 * @return the orientation
+	 * @return The Exif orientation or {@code 1} if unknown.
 	 * @since 1.50.0
 	 */
-	public int getOrientation() {
-		return imageInfo != null ? imageInfo.getExifOrientation() : 0;
+	public ExifOrientation getExifOrientation() {
+		return imageInfo != null ? imageInfo.getExifOrientation() : ExifOrientation.TOP_LEFT;
 	}
 
 	/**
